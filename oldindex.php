638ec4ba727d5188bdba80d6e6c63756c182a2bd
Thejesh Bhagavanth <thejeshbhagavanth@gmail.com>
2024-01-10 19:14:52 +0530

    updation

--- oldindex.php
+++ oldindex.php
@@ -69,6 +69,10 @@ include_once("config.php");
             </div>
         </div>
     </div>
+    <script>
+        var date = new Date();
+        document.getElementById("date").value = date.toISOString().substr(0, 10);
+    </script>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL"

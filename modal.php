2de8f2142a9a839fdcfdcabc36797ac5f0e5f7a3
Thejesh Bhagavanth <thejeshbhagavanth@gmail.com>
2024-01-18 11:33:14 +0530

    modal works properly

--- modal.php
+++ modal.php
@@ -18,7 +18,7 @@
     </button>
 
     <!-- Modal -->
-    <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal fade" id="" tabindex="-1" >
         <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
             <div class="modal-content">
                 <div class="modal-header">
